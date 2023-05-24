67ef6e15fada999ddaabb9b1d493cb95543af052
djfksdl <djfksdl@naver.com>
2023-05-24 22:15:26 +0900

    수정중

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".thumb_Swiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".visual_Swiper", {
+    speed: 5000,
+    // autoplay: {
+    //       delay: 5000,
+    //       disableOnInteraction: false
+    //     },
+    loop: true,
+    spaceBetween: 0,
+    navigation: {
+      // nextEl: ".visual_Swiper-button-next",
+      // prevEl: ".visual_Swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
